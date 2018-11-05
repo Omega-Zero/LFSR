19438244a9a1cea845a5fe6fe1911a73458ed757
Omega-Zero <36581543+Omega-Zero@users.noreply.github.com>
2018-11-05 13:52:10 -0500

    Initial commit.

---
+++ src/miniDES/DESinputs.java
@@ -0,0 +1,27 @@
+												//Jeffrey Marple Oct 2018//
+package miniDES;
+
+import java.util.Scanner;
+
+//program that asks for user input
+public class DESinputs {
+	public int parameterOne;
+	public int parameterTwo;
+	
+	public static void main(String [] args){
+
+
+			Scanner userInputs = new Scanner(System.in);
+//First param Input			
+			System.out.println("Select first LFSR parameter");
+			int parameterOne = userInputs.nextInt();
+			System.out.println("First parameter set to "+ parameterOne);
+//Second param input
+			System.out.println("Select second LFSR parameter");
+			int parameterTwo = userInputs.nextInt();
+			userInputs.close();
+			System.out.println("Second parameter set to "+ parameterTwo);
+			
+		}
+		int XOR = new XORprogram().XORtruth(parameterOne, parameterTwo);
+	}
