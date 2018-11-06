9abc0fee3dd054bab652707045cdf9165d0baf7e
Omega-Zero <36581543+Omega-Zero@users.noreply.github.com>
2018-11-06 09:22:44 -0500

    XORprogram bit tests

--- src/miniDES/XORprogram.java
+++ src/miniDES/XORprogram.java
@@ -8,7 +8,14 @@ public class XORprogram{
 	public static int XORtruth (int paramOne, int paramTwo, int bitOne , int bitTwo) {
 	
 		int result;
-	
+//Test to make sure bits are valid
+		if (bitOne!=1 && bitOne !=0) {
+			System.out.println("Bit one is not binary");
+		}
+		if (bitTwo!=1 && bitTwo !=0) {
+			System.out.println("Bit two is not binary");
+		}
+//truth table for XOR		
 		if(bitOne== 1 && bitTwo ==1) {
 			result=0;
 		}
